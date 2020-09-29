86c329ef1516eab83e0a55294ad4563797045bcc
Tamir Cherniak <tamircher123@gmail.com>
2020-09-29 13:08:35 +0300

    init

---
+++ promise-map-template.js
@@ -0,0 +1,32 @@
+var Promise = require("bluebird");
+
+var t0;
+
+function exitHandler() {
+  const t1 = new Date();
+  console.log("time", (t1 - t0) / 1000);
+
+  // insert logic to handle at exit
+
+  process.exit(0);
+}
+
+process.on("SIGINT", exitHandler);
+
+(async function main() {
+  try {
+    const array = [];
+    t0 = new Date();
+    await Promise.map(
+      array,
+      (entry, index) => {
+        // return promise here
+      },
+      { concurrency: 50 },
+    );
+  } catch (error) {
+    console.log(error);
+  } finally {
+    exitHandler();
+  }
+})();
